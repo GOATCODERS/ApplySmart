481f98059db0960a13f19d3e4eb4b0d2b55a931b
Thokozani Masanabo <thokozani26sngalela@gmail.com>
2024-07-29 10:36:47 +0200

    modified and added the register code

--- base.php
+++ base.php
@@ -49,35 +49,9 @@
           </div>
       </div>
   </nav>
-
-      <div class="d-flex h-100 " width="100" height="100" style="background-image: url('assets/background2.jpeg'); background-position: center; background-size: cover;">
-        <!-- Content here -->
- 
-        <div class="card text-bg-success " style="width: 25rem; --bs-bg-opacity: 0.3; height: 17rem; margin: 120px 120px;">
-          <div class="card-body text-center">
-            <img src="assets/logo.png" data-bs-theme="dark" alt="Logo" width="60" height="80" class="d-inline-block align-text-top" style="margin-left: 10px; margin-right: 5px;">
-            <div>
-              <h1 class="card-title">Apply Smart</h1>
-            </div>
-            
-            <div>
-              <p class="card-text">Find Your Ideal Course In Just A few Clicks.</p>
-            </div>
-            
-            <div class="my-3">
-              <a class="btn btn-success" href="signupPage.html" role="button">Create a Profile</a>
-              <a class="btn btn-outline-success" href="loginPage.html" role="button">Login</a>
-            </div>
-          </div>
-      
-        </div>
-        
-
-      </div>
-
-  
-      
-  
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
+  <?php
+    include 'Prospectus.html';
+  ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
 </html>
